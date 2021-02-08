63747cee35785bce8f3a2408a169bc71a3626f73
claire <clairebontempo@gmail.com>
2021-02-07 20:07:45 -0800

    commit tree bca06f9e2c2e3b87f47c6410f39ba7047cb83fba

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array){
+  // array.find(function(object){ return object["result"] === "W" })
+  let win = array.find( ({result}) => result === "W")
+  return win ? win.year : undefined
+//  if (win){
+//    return win.year
+//  }else {
+//    return undefined
+//   }
+}
+  
